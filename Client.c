10d1e59fcb980862b9ee6bb060395456feeb6e8f
adevkp <adevkp@icloud.com>
2023-04-17 16:47:43 +0530

    specific print messages have been added

--- Client.c
+++ Client.c
@@ -1,16 +1,40 @@
-#include<stdio.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/types.h>
-#include<string.h>
-#include<errno.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <time.h>
+#define PRINT_INFO(MSG, ...)                                                          \
+    {                                                                                 \
+        setenv("TZ", "Asia/Kolkata", 1);                                              \
+        tzset();                                                                      \
+        time_t current_time = time(NULL);                                             \
+        printf("%s INFO %d:%d %ld %s %s %d : " MSG "\n",                              \
+               __TIME__, getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+               __LINE__, ##__VA_ARGS__);                                              \
+    }
+#define PRINT_ERROR(MSG, ...)                                                         \
+    {                                                                                 \
+        setenv("TZ", "Asia/Kolkata", 1);                                              \
+        tzset();                                                                      \
+        time_t current_time = time(NULL);                                             \
+        printf("%s ERROR %d:%d %ld %s %s %d : " MSG "\n",                             \
+               __TIME__, getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+               __LINE__, ##__VA_ARGS__);                                              \
+    }
 
 #define NAME_SIZE 256
 #define BUF_SIZE 1024
-#define SHM_KEY 0x1234 //Used for conntection channel
+#define SHM_KEY 0x1234 // Used for conntection channel
 
-struct conSeg{
+struct conSeg
+{
     int lock;
     int shid;
     int id;
@@ -23,21 +47,21 @@ struct resSeg
     char op[BUF_SIZE];
 };
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     int id = 200;
     char name[NAME_SIZE];
-    printf("\nEnter a client name:");
+    PRINT_INFO("\nEnter a client name:");
     scanf("%s", &name[0]);
 
-    printf("Client name : {%s} Length {%d}\n", name, strlen(name));
+    PRINT_INFO("Client name : {%s} Length {%ld} \n", name, strlen(name));
 
     int conSegID;
     int resSegID;
 
-    if((conSegID = shmget(SHM_KEY, sizeof(struct conSeg), 0)) == -1)
+    if ((conSegID = shmget(SHM_KEY, sizeof(struct conSeg), 0)) == -1)
     {
-        perror("Shared memory Not found, please start the server first\n");
+        PRINT_ERROR("Shared memory Not found, please start the server first");
         return 1;
     }
 
@@ -45,8 +69,9 @@ int main(int argc, char *argv[])
 
     // Attach to the segment to get a pointer to it.
     conSeg = shmat(conSegID, NULL, 0);
-    if (conSeg == (void *) -1) {
-        perror("Shared memory attach");
+    if (conSeg == (void *)-1)
+    {
+        PRINT_ERROR("Shared memory attach");
         return 1;
     }
 
@@ -55,31 +80,30 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        //Adding a request
-        if(registered == 0)
+        // Adding a request
+        if (registered == 0)
         {
-            if(conSeg->lock != 0 
-            || conSeg->id != 0) //In case there is already a connection request
+            if (conSeg->lock != 0 || conSeg->id != 0) // In case there is already a connection request
                 continue;
             conSeg->lock = 2;
-            printf("Adding connection Request....\n");
+            PRINT_INFO("Adding connection Request....\n");
             conSeg->id = id;
 
             for (int i = 0; i < strlen(name); i++)
             {
                 conSeg->name[i] = name[i];
             }
-            
+
             registered = 1;
             conSeg->lock = 0;
         }
 
-        //Waiting for response
-        if(registered == 1)
+        // Waiting for response
+        if (registered == 1)
         {
-            if(conSeg->lock != 3)
+            if (conSeg->lock != 3)
             {
-                //printf("Waiting for response....\n");
+                // printf("Waiting for response....\n");
                 continue;
             }
             conSeg->lock = 2;
@@ -90,10 +114,10 @@ int main(int argc, char *argv[])
             registered++;
             conSeg->lock = 0;
         }
-        printf("Response value : %d \n", resSeg->a);
+        PRINT_INFO("Response value : %d \n", resSeg->a);
         sleep(1);
     }
-    printf("Exiting client\n", conSeg->lock);
+    PRINT_INFO("Exiting client %d \n", conSeg->lock);
 
     return 0;
 }
--- Server.c
+++ Server.c
@@ -1,32 +1,55 @@
-#include<stdio.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/types.h>
-#include<string.h>
-#include<errno.h>
-#include<stdlib.h>
-#include<signal.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <pthread.h>
+#include <time.h>
+
+#define PRINT_INFO(MSG, ...)                                                          \
+    {                                                                                 \
+        setenv("TZ", "Asia/Kolkata", 1);                                              \
+        tzset();                                                                      \
+        time_t current_time = time(NULL);                                             \
+        printf("%s INFO %d:%d %ld %s %s %d : " MSG "\n",                              \
+               __TIME__, getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+               __LINE__, ##__VA_ARGS__);                                              \
+    }
+#define PRINT_ERROR(MSG, ...)                                                         \
+    {                                                                                 \
+        setenv("TZ", "Asia/Kolkata", 1);                                              \
+        tzset();                                                                      \
+        time_t current_time = time(NULL);                                             \
+        printf("%s ERROR %d:%d %ld %s %s %d : " MSG "\n",                             \
+               __TIME__, getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+               __LINE__, ##__VA_ARGS__);                                              \
+    }
 
 #define NAME_SIZE 256
 #define BUF_SIZE 1024
-#define SHM_KEY 0x1234 //Used for conntection channel
+#define SHM_KEY 0x1234 // Used for conntection channel
 #define MAX_THREADS 100
 
-int shmid; //Connection shared memory id
+int shmid; // Connection shared memory id
 pthread_t process[MAX_THREADS];
-int num; //Current number of worker threads
-int childShId[MAX_THREADS]; //Shared memory id for each child worker/client
+int num;                    // Current number of worker threads
+int childShId[MAX_THREADS]; // Shared memory id for each child worker/client
 
-//Struct for connection request
-struct conSeg{
+// Struct for connection request
+struct conSeg
+{
     int lock; // 0-> no lock, 1 -> locked by server, 2 -> locked by client, 3 -> response from server
     int shid;
     int id;
     char name[NAME_SIZE];
 };
 
-//Struct for indivisual responses
+// Struct for indivisual responses
 struct resSeg
 {
     int a;
@@ -36,19 +59,19 @@ struct resSeg
 
 void handle_sigint(int sig)
 {
-    printf("\nClosing shared memory segment.....\n");
-    
-    //Close connection Shared memory
+    PRINT_INFO("\nClosing shared memory segment.....\n");
+
+    // Close connection Shared memory
     shmctl(shmid, IPC_RMID, NULL);
-    
-    //Close response Shared memory for each client
+
+    // Close response Shared memory for each client
     for (int i = 0; i < num; i++)
     {
         shmctl(childShId[i], IPC_RMID, NULL);
     }
-    
+
     printf("Closing worker Threads....\n");
-    //Close worker Threads
+    // Close worker Threads
     for (int i = 0; i < num; i++)
     {
         kill(*(process + i), SIGKILL);
@@ -67,26 +90,27 @@ int hash(unsigned char *str)
     return hash;
 }
 
-void* ResponseThread(void* d)
+void *ResponseThread(void *d)
 {
-    int id = *(int*)d;
-    printf("\nWorker Thread Executing : {%d}\n", id);
-    
+    int id = *(int *)d;
+    PRINT_INFO("\nWorker Thread Executing : {%d}\n", id);
+
     struct resSeg *resSeg;
     resSeg = shmat(id, NULL, 0);
-    //Just a sample response
-    while(1){
+    // Just a sample response
+    while (1)
+    {
         resSeg->a += resSeg->b;
         sleep(1);
     }
     return d;
 }
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    if((shmid = shmget(SHM_KEY, sizeof(struct conSeg), 0644|IPC_CREAT)) == -1)
+    if ((shmid = shmget(SHM_KEY, sizeof(struct conSeg), 0644 | IPC_CREAT)) == -1)
     {
-        perror("Shared memory");
+        PRINT_ERROR("Shared memory");
         return 1;
     }
 
@@ -94,24 +118,25 @@ int main(int argc, char *argv[])
 
     // Attach to the segment to get a pointer to it.
     conSeg = shmat(shmid, NULL, 0);
-    if (conSeg == (void *) -1) {
-        perror("Shared memory attach");
+    if (conSeg == (void *)-1)
+    {
+        PRINT_ERROR("Shared memory attach");
         return 1;
     }
-    
+
     conSeg->id = 0;
-	signal(SIGINT, handle_sigint);
+    signal(SIGINT, handle_sigint);
 
     while (1)
     {
-        if(conSeg->lock != 0)
+        if (conSeg->lock != 0)
             continue;
-        conSeg-> lock = 1;
-        if(conSeg->id != 0)
+        conSeg->lock = 1;
+        if (conSeg->id != 0)
         {
-            printf("New Connection Request");
-            childShId[num] = shmget(hash(conSeg->name), sizeof(struct resSeg), 0644|IPC_CREAT);
-            process[num] = pthread_create(&process[num], NULL, ResponseThread, (void*)&childShId[num]);
+            PRINT_INFO("New Connection Request");
+            childShId[num] = shmget(hash(conSeg->name), sizeof(struct resSeg), 0644 | IPC_CREAT);
+            process[num] = pthread_create(&process[num], NULL, ResponseThread, (void *)&childShId[num]);
             conSeg->id = 0;
             conSeg->shid = childShId[num];
             num++;
@@ -120,6 +145,6 @@ int main(int argc, char *argv[])
         }
         conSeg->lock = 0;
     }
-    
+
     return 0;
 }
